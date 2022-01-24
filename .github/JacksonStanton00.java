6b6617df3a4f3959af6aa0e486b9a236928637df
Amari L <70532194+S11013885@users.noreply.github.com>
2022-01-24 08:48:27 -0500

    v0.1

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -1,4 +1,10 @@
+// Amari Lombel, Arry pratice , v0.1 , 1/24/22
+
 public class JacksonStanton00
-String test[] = newString[10];
-double test[] = newdouble[4];
-int test[] = newint[5];
+{
+public static void main(string[]args){
+String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
+double[] myBankAccount ={0.0, 45.89, 4234.34, 35.35, 0.1} ;
+int[] myGrades = {100, 85 , 70 , -40 ,0};
+}
+}
