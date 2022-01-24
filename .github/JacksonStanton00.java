2ee79726d713970a05501772bf68d2f8caa48334
Amari L <70532194+S11013885@users.noreply.github.com>
2022-01-24 09:34:19 -0500

    v0.5
    added each element1 and 2 for the double and int.

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -2,10 +2,9 @@
 
 public class JacksonStanton00
 {
-public static void main(string[]args){
-String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
-double[] myBankAccount ={0.0, 45.89, 4234.34, 35.35, 0.1} ;
-int[] myGrades = {100, 85 , 70 , -40 ,0};
+public static void main(String[] args){
+
+
 /*
 for (current_element = 0 ; current_element < carArr.lentgh(); current_element += 1;) {
     // code to execute
@@ -17,10 +16,23 @@ for( i = 0; i < carArr.lentgh(); i++){
 }
 */
 String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
+System.out.println(carArr[3]);
+//once created you cannot change the size of an array. IT IS FORRBIDDEN
+carArr[0] = "Volkswagen Passat";
 for (String eachElement : carArr) {
     //code to execute
     System.out.println(eachElement);
+double[] myBankAccount ={0.0, 45.89, 4234.34, 35.35, 0.1} ;
+for (double eachElement1 : myBankAccount) {
+    //code to execute
+    System.out.println(eachElement1);
+int[] myGrades = {100, 85 , 70 , -40 ,0};
+for (int eachElement2 : myGrades) {
+    //code to execute
+    System.out.println(eachElement2);
+
 
 }
+    }
 }
 }
