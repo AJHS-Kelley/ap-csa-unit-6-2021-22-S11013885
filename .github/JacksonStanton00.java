834f34a8f4e907c00dc6c2efc0007dfcd0d0cdb5
Amari L <70532194+S11013885@users.noreply.github.com>
2022-01-24 09:25:40 -0500

    v0.2

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -6,5 +6,21 @@ public static void main(string[]args){
 String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
 double[] myBankAccount ={0.0, 45.89, 4234.34, 35.35, 0.1} ;
 int[] myGrades = {100, 85 , 70 , -40 ,0};
+/*
+for (current_element = 0 ; current_element < carArr.lentgh(); current_element += 1;) {
+    // code to execute
+    // statement 1 is executed once before the loop
+    // statment 2 is the contional for the loop.
+    //statement 3 is executed eac time the loop runs.
+}
+for( i = 0; i < carArr.lentgh(); i++){
+}
+*/
+String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
+for (String eachElement : carArr) {
+    //code to execute
+    System.out.println(eachElement);
+
+}
 }
 }
