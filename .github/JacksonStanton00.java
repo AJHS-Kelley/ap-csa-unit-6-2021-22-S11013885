1a331d95593742301299b7571e4eb17a1180f281
Amari L <70532194+S11013885@users.noreply.github.com>
2022-01-26 08:35:24 -0500

    Update JacksonStanton00.java

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -4,7 +4,7 @@ public class JacksonStanton00
 {
 public static void main(String[] args){
 
-
+String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
 /*
 for (current_element = 0 ; current_element < carArr.lentgh(); current_element += 1;) {
     // code to execute
@@ -12,12 +12,22 @@ for (current_element = 0 ; current_element < carArr.lentgh(); current_element +=
     // statment 2 is the contional for the loop.
     //statement 3 is executed eac time the loop runs.
 }
-for( i = 0; i < carArr.lentgh(); i++){
-}
 */
-String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
+for( int i = 0; i < carArr.lentgh; i++){
+    // System.out.println(i);
+    System.out.println(carArr[i]);
+
+}
+
+
+
+
+
+
+
 System.out.println(carArr[3]);
 //once created you cannot change the size of an array. IT IS FORRBIDDEN
+// Enhanced For Loop
 carArr[0] = "Volkswagen Passat";
 for (String eachElement : carArr) {
     //code to execute
@@ -32,7 +42,22 @@ for (int eachElement2 : myGrades) {
     System.out.println(eachElement2);
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
     }
-}
 }
