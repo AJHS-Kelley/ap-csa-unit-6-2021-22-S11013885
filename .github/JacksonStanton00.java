5ce957766600d4d719ec65e49cb09644ae96ffc0
Amari L <S11013885@students.duvalschools.org>
2022-01-28 09:30:36 -0500

    v.06 5b

--- .github/JacksonStanton00.java
+++ .github/JacksonStanton00.java
@@ -4,7 +4,7 @@ public class JacksonStanton00
 {
 public static void main(String[] args){
 
-String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
+// String[] carArr ={"BMW M5", "Ferrari 250 GTO", "Porsche 911", "Ford F150 Lighting", "Buick Skylark"};
 /*
 for (current_element = 0 ; current_element < carArr.lentgh(); current_element += 1;) {
     // code to execute
@@ -13,6 +13,7 @@ for (current_element = 0 ; current_element < carArr.lentgh(); current_element +=
     //statement 3 is executed eac time the loop runs.
 }
 */
+/*
 for( int i = 0; i < carArr.length; i++){
     // System.out.println(i);<--- print the index variable
     // System.out.println(carArr[i]);<----
@@ -31,9 +32,9 @@ for( int i = 0; i < carArr.length; i++){
 
 }
 
+*/
 
-
-
+/*
 System.out.println(carArr[3]);
 //once created you cannot change the size of an array. IT IS FORRBIDDEN
 // Enhanced For Loop
@@ -49,6 +50,22 @@ int[] myGrades = {100, 85 , 70 , -40 ,0};
 for (int eachElement2 : myGrades) {
     //code to execute
     System.out.println(eachElement2);
+*/
+
+// 2D array
+int[][] twoDIntArr= {{4,1,2,-5},{8,9,25,-4}};
+
+System.out.println(twoDIntArr[0][2]);
+System.out.println(twoDIntArr[1][3]);
+
+// nested for loop
+ for (int i = 0; i < twoDIntArr; ++i ){
+     for (int j = 0; < twoDIntArr[i].length; ++j ){
+         System.out.println(twoDIntArr[i][j]);
+     }
+
+ }
+
 
 
 
@@ -68,7 +85,7 @@ for (int eachElement2 : myGrades) {
 
 }
 }
-}
-}
-}
-}
+//}
+//}
+//}
+//}
